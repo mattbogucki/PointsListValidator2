cd86c9a331bfd8fec3d86a732a79f4060a5ee37a
Matthew.Bogucki <matt.bogucki.829@gmail.com>
2023-02-09 11:23:17 -0800

    Merged into Master

--- test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-expiration_date = datetime.datetime.strptime("1/1/2023", "%m/%d/%Y")
-todays_date = datetime.datetime.now()
-
-print(todays_date > expiration_date)
-
-print('None'.split("_")[-1])
